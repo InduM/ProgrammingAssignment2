3c93bd0936057c7fb08a8ea0d827662c6ee006a1
Indu <indu3102@gmail.com>
2014-12-17 20:25:51 +0530

    alignments modified

--- cachematrix.R
+++ cachematrix.R
@@ -10,33 +10,32 @@
 
 makeCacheMatrix <- function(x = matrix())  
 {	
-        IM <- NULL							#variable 'IM' is for the inverted matrix
-											#IM is NULL when makeCacheMatrix is called for the first time
-       
-		set <- function(y)					# function takes the input matrix
-		{
+        IM <- NULL						#variable 'IM' is for the inverted matrix
+								#IM is NULL when makeCacheMatrix is called for the first time
+      	set <- function(y)					# function takes the input matrix
+	{
                 x <<- y						#saves the input matrix
                 IM <<- NULL					#initializes the inverse matrix to NULL
         }
        
-		getmatrix <- function()	
-		{								
-			x								#This function returns the input matrix
-		}
+	getmatrix <- function()	
+	{								
+		x						#This function returns the input matrix
+	}
         
-		setinverse <- function(inverse) 	#this is called by cachmatrix() during the first 
-		{									#cachematrix() access and stores the value using superassignment
-			IM <<- inverse				
-		}
+	setinverse <- function(inverse) 			#this is called by cachmatrix() during the first 
+	{							#cachematrix() access and stores the value using superassignment
+		IM <<- inverse				
+	}
         
-		getinverse <- function() 			# returns the cached value to cachemean() on
-        {							  		#  subsequent accesses
-			IM
-		}
+	getinverse <- function()	 			# returns the cached value to cachemean() on
+        {					  		#  subsequent accesses
+		IM
+	}
        
-	   list(getmatrix = getmatrix,  		# a list of the internal functions is returned each time   
-             setinverse =  setinverse,	 	# makeCacheMatrix is invoked so a calling function
-			 getinverse = getinverse)	 	# knows how to access those methods.                            
+	list(getmatrix = getmatrix,  				# a list of the internal functions is returned each time   
+             setinverse =  setinverse,	 			# makeCacheMatrix is invoked so a calling function
+             getinverse = getinverse)			 	# knows how to access those methods.                            
 }
 
 
@@ -44,21 +43,21 @@ makeCacheMatrix <- function(x = matrix())
 #the "matrix" returned by `makeCacheMatrix` .
 cacheSolve <- function(x,...)
 {
-		m <- x$getinverse()						#gets inverse form makeCacheMatrix and assigns to m
+	m <- x$getinverse()					#gets inverse form makeCacheMatrix and assigns to m
        
-	   if(!is.null(m))							#checks if the inveerse is null i.e. checks if the inverse
-		{										#has been computed already and cached
-			message("getting cached data")
-            return(m)							# returns the cached inverse
+      	if(!is.null(m))						#checks if the inveerse is null i.e. checks if the inverse
+	{							#has been computed already and cached
+		message("getting cached data")
+         	return(m)					# returns the cached inverse
         }
         
-		data <- x$getmatrix()					#gets the input matrix from makeCacheMatrix function 
-												#and assigns to the variable data
+	data <- x$getmatrix()					#gets the input matrix from makeCacheMatrix function 
+								#and assigns to the variable data
         
-		m <- solve(data,...)					#Finds the inverse of a matrix
+	m <- solve(data,...)					#Finds the inverse of a matrix
        
-	   x$setinverse(m)							#caches the inverse matrix for future use 
+	x$setinverse(m)						#caches the inverse matrix for future use 
 
-	   m									 	# Return a matrix that is the inverse of 'x'
+	m							# Return a matrix that is the inverse of 'x'
 }
 
